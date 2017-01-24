f0a0581e4cb38f42da77fb77016cec59dbbd4c6c
Todd Baert <toddbaert@gmail.com>
2017-01-24 10:40:20 -0500

    further making install.sh agnostic

--- scripts/install.js
+++ scripts/install.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const browserify = require('browserify');
 const exec = require('child_process').exec;
-const jsDir = 'public/js';
+const path = require('path');
+
+const publicDir = 'public';
+const jsDir = 'js';
+const rootDir = __dirname.split(path.sep).slice(0, __dirname.split(path.sep).length - 1).join(path.sep);
 
 console.log('echo running browserify job, and compiling JSX...');
-if (!fs.existsSync(jsDir)){
-  fs.mkdirSync(jsDir);
+if (!fs.existsSync(rootDir + path.sep + publicDir)){
+  fs.mkdirSync(rootDir + path.sep + publicDir);
+  fs.mkdirSync(rootDir + path.sep + publicDir + path.sep + jsDir);
 }
 exec('browserify -t [ babelify --presets [ react ] ] client/src/js/pulsar.js -o public/js/bundle.js', function (error, stdout, stderr) {
   if (error !== null) {
